666b4895a35b042783f732cff1914feffff76800
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-22 16:24:59 -0500

    Playing around with layers

--- python/deeplatent.py
+++ python/deeplatent.py
@@ -7,19 +7,19 @@ import torch.nn.functional as F
 
 class DeepLatentNN(nn.Module):
 
-    def __init__(self, n_users, n_movies, H1 =30, D_out = 10):
+    def __init__(self, n_u, H1 =50, D_out = 10):
         super(DeepLatentNN, self).__init__()
         
-        self.layer1 = nn.Embedding(757,50)
-        self.layer2 = nn.Linear(50,H1)
-        self.layerOut   = nn.Linear(H1, 1)
+        self.layer1 = nn.Embedding(n_u,10)
+        self.layer2 = nn.Linear(10,H1)
+        self.layerOut   =  nn.Linear(H1, 1)
         
 
 
 
     def forward(self, x):
            x_h =  self.layer1(x)
-           x_h2 = self.layer2(x_h)
+           x_h2 = F.sigmoid(self.layer2(x_h))
            preds = self.layerOut(x_h2)
 
            print(preds)
