4a58507c5ffd2a41ac2ddd251d8d993b24698256
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-22 15:27:09 -0500

    add data and build pytorch skeleton

--- python/deeplatent.py
+++ python/deeplatent.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,19 +7,26 @@ import torch.nn.functional as F
 
 class DeepLatentNN(nn.Module):
 
-    def __init__(self, n_users, n_movies, n_factors, H1, D_out):
+    def __init__(self, n_users, n_movies, H1 =30, D_out = 10):
         super(DeepLatentNN, self).__init__()
-        self.layer = nn
-        self.layer = nn
+        
+        self.layer1 = nn.Embedding(757,50)
+        self.layer2 = nn.Linear(50,H1)
+        self.layerOut   = nn.Linear(H1, 1)
+        
 
 
 
     def forward(self, x):
-        prediction_matrix = self.out
+           x_h =  self.layer1(x)
+           x_h2 = self.layer2(x_h)
+           preds = self.layerOut(x_h2)
 
-        return prediction_matrix
+           print(preds)
 
-    def predict(self, x):
-        # return the score
-        output_scores = self.forward(x)
-        return output_scores
+           return preds
+
+
+
+
+
