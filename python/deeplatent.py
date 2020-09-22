9663fee8e0e85ae264e9ec6bb356cff4764208a1
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-21 21:56:40 -0500

    Update deeplatent.py

--- python/deeplatent.py
+++ python/deeplatent.py
@@ -7,12 +7,15 @@ import torch.nn.functional as F
 class DeepLatentNN(nn.Module):
 
     def __init__(self):
-
+        
 
     
 
 
     def forward(self, x):
 
+
+
+        return prediction_matrix
 
 
