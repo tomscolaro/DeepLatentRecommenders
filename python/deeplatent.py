7a4965f955c8a05a46a55e32e21f760ac0867d6e
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-23 14:57:57 -0500

    changes to model

--- python/deeplatent.py
+++ python/deeplatent.py
@@ -7,24 +7,29 @@ import torch.nn.functional as F
 
 class DeepLatentNN(nn.Module):
 
-    def __init__(self, n_u, H1 =50, D_out = 10):
+    def __init__(self, n_u, H1 =50, D_out = 50):
         super(DeepLatentNN, self).__init__()
-        
-        self.layer1 = nn.Linear(n_u,10000)
-        self.layer2 = nn.Linear(10000,H1)
+
+
+        self.layerEncode = nn.Embedding(n_u,50
+        self.layer1 = nn.Linear(3,10)
+
+        self.layer2 = nn.Linear(50,H1)
         self.layerOut   =  nn.Linear(H1, 1)
         
 
 
 
-    def forward(self, x):
-           x_h =  self.layer1(x)
-           x_h2 = F.sigmoid(self.layer2(x_h))
-           preds = self.layerOut(x_h2)
+    def forward(self, x):  
+        
+        x_h = self.layerEncode(x)
+        #x_h =  torch.cat((self.layerEncode(x[:,0]), self.layer1(x[:,1:])),1)
+        x_h2 = F.sigmoid(self.layer2(x_h))
+        preds = self.layerOut(x_h2)
 
-           print(preds)
+        print(preds)
 
-           return preds
+        return preds
 
 
 
