5b30fbd827110c470a436ea628fd0ea724ad3f03
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-24 15:45:12 -0500

    Changes to deeplatent.py

--- python/deeplatent.py
+++ python/deeplatent.py
@@ -11,13 +11,14 @@ class DeepLatentNN(nn.Module):
         super(DeepLatentNN, self).__init__()
         self.layerEncodeU = nn.Embedding(n_users ,n_factors,sparse=True)
         self.layerEncodeM = nn.Embedding(n_movies,n_factors,sparse=True)
-        self.layerEncodeUB = torch.nn.Embedding(n_users, 1, sparse=True)
-        self.layerEncodeMB = torch.nn.Embedding(n_movies, 1, sparse=True)
+        self.layerEncodeUB = nn.Embedding(n_users, 1, sparse=True)
+        self.layerEncodeMB = nn.Embedding(n_movies, 1, sparse=True)
 
-    
     def forward(self, x1, x2):  
         preds = self.layerEncodeUB(x1) + self.layerEncodeMB(x2)
+        
         preds += (self.layerEncodeU(x1) * self.layerEncodeM(x2)).sum(dim=1, keepdim=True)
+        preds = torch.clamp(preds,0,5)
         return preds.squeeze()
         
 
